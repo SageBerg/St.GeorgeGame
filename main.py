f80fe8d722249eb621ce8e6ba6b0603337f09538
Sage Berg <sberg@uoregon.edu>
2014-12-07 21:26:52 -0800

    set up main loop and made the game automatically resart after it ends

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ St. George Game
 main.py
 Sage Berg
 Created: 5 Dec 2014
-Updated: 5 Dec 2014
+Updated: 7 Dec 2014
 """
 
-from stgoergegame import *
+from stgeorgegame import *
 
 
 def main():
+    character = Character()
+    place = None
+    person = None
+    prev_act = None
+    while character.alive and character.alone:
+        curr_frame = Frame(place, person, prev_act)
+        location, person, prev_act = curr_frame.prompt()
+
+    """
     tavern = Place("the tavern")
     streets = Place("the streets")
     church = Place("the church")
@@ -17,10 +26,9 @@ def main():
     marketplace = Place("the marketplace")
     countryside = Place("the countryside")
     prison = Place("the prison")
+    """
 
-
-def get_options():
-    pass
 
 if __name__ == "__main__":
-    main()
+    while True:  # the game automatically restarts
+        main()
