abb875dbd37a14e7becd5f7c7dafc4465c48deee
Sage Berg <sberg@uoregon.edu>
2014-12-08 21:54:54 -0800

    changed strategy for initializing first frame

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ from places import TavernStart
 def main():
     character = Character()
     msg = "You are in a tavern. The local assassins hate you."
-    opening_frame = Frame(msg, TavernStart, None, None)
+    opening_frame = Frame(msg, Tavern, None, None)
+    opening_frame.a_action = AskAboutAssassins
+    opening_frame.b_action = BuyADrink 
+    opening_frame.c_action = LeaveInAHuff 
+    opening_frame.d_action = SingASong 
     opening_frame.prompt()
     while character.alive and character.alone:
         curr_frame = Frame(place, person, prev_act)
