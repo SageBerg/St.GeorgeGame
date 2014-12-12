860076c326cfc316f9494855b0dc23bdf5ad06ec
Sage Berg <sberg@uoregon.edu>
2014-12-11 21:20:46 -0800

    added PrettyLady, class not done, needs to add more actions

--- persons.py
+++ persons.py
@@ -1,16 +1,44 @@
 """
+St. George Game
 person.py
 Sage Berg
 Created: 7 Dec 2014
 """
+
+from actions import *
+from action_bag import ActionBag
+import abc
 
 
 class Person(object):
     """
     abstract class
     """
+    __metaclass__ = abc.ABCMeta
 
-    def __init__(self, name, attack):
+    @abc.abstractmethod
+    def __init__(self, name, attack, pronoun):
         self.name = name
         self.attack = attack
-        self.pronoun = "it"
+        self.pronoun = pronoun
+
+    @abc.abstractmethod
+    def options(self):
+        "returns an options_bag"
+        pass
+
+
+class PrettyLady(Person):
+
+    def __init__(self, Attack):
+        self.name = "pretty lady"
+        self.attack = 1
+        self.pronouns = ("she", "her")
+        self.options = {"a": ActionBag(),
+                        "b": ActionBag(),
+                        "c": ActionBag(),
+                        "d": ActionBag()}
+        self.options["a"].add(Attack())  # broken
+        #self.options["b"].add(BoastOfYourBravery)
+        #self.options["b"].add(BuyHerADrink)
+        #self.options["c"].add(TalkAboutCats)
