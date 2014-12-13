74386054badc346dbb82a39d13cf05d2ac29baf1
Sage Berg <sberg@uoregon.edu>
2014-12-12 22:23:45 -0800

    added all weapons

--- weapons.py
+++ weapons.py
@@ -8,4 +8,12 @@ Created: 12 Dec 2014
 from collections import namedtuple
 
 Weapon = namedtuple("Weapon", ["name", "attack", "price"])
+
+pitchfork = Weapon("pitchforkr", 1, "pittance")
 dagger = Weapon("dagger", 2, "pittance")
+cutlass = Weapon("cutlass", 3, "pittance")
+hammer = Weapon("hammer", 4, "pittance")
+long_pitchfork = Weapon("long pitchfork", 5, "pittance")
+poisoned_dagger = Weapon("poisoned dagger", 6, "small fortune")
+jeweled_cutlass = Weapon("jeweled cutlass", 7, "large fortune")
+iron_hammer = Weapon("dagger", 8, "small fortune")
