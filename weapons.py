bce8f3c274884d3166b2b5a0a2191094a37993c6
Sage Berg <sagetberg@gmail.com>
2014-12-13 21:59:59 -0800

    put the weapons into a list

--- weapons.py
+++ weapons.py
@@ -9,11 +9,12 @@ from collections import namedtuple
 
 Weapon = namedtuple("Weapon", ["name", "attack", "price"])
 
-pitchfork = Weapon("pitchforkr", 1, "pittance")
-dagger = Weapon("dagger", 2, "pittance")
-cutlass = Weapon("cutlass", 3, "pittance")
-hammer = Weapon("hammer", 4, "pittance")
-long_pitchfork = Weapon("long pitchfork", 5, "pittance")
-poisoned_dagger = Weapon("poisoned dagger", 6, "small fortune")
-jeweled_cutlass = Weapon("jeweled cutlass", 7, "large fortune")
-iron_hammer = Weapon("dagger", 8, "small fortune")
+weapons = [
+Weapon("pitchfork", 1, "pittance"),
+Weapon("dagger", 2, "pittance"),
+Weapon("cutlass", 3, "pittance"),
+Weapon("hammer", 4, "pittance"),
+Weapon("long pitchfork", 5, "pittance"),
+Weapon("poisoned dagger", 6, "small fortune"),
+Weapon("jeweled cutlass", 7, "large fortune"),
+Weapon("dagger", 8, "small fortune") ]
