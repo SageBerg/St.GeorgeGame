c115dd3cf558c4a46abdfb61aa95ae3599b2a6ac
Sage Berg <sagetberg@gmail.com>
2014-12-14 21:30:16 -0800

    refactored AskAboutAssassins to operate under Skyler's new format for actions, added several actions, allowed actions to influence which actions will be available next

--- actions.py
+++ actions.py
@@ -40,6 +40,10 @@ class copy_paste(Action):
 
     def __init__(self):
         self.name = ""
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         pass
@@ -52,23 +56,41 @@ class AskAboutAssassins(Action):
 
     def __init__(self):
         self.name = "Ask about assassins."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
-        roll = random.randint(0, 9)
-        if roll < 0:
+        def assassinated():
             Display().write("The first person you ask about assassins turns out to be an assassin. She assassinates you.")
             character.die()
-        elif roll == 6 or roll == 7 or roll < 10:
+
+        def pretty_lady():
             Display().write("During your search, you strike up a conversation with a pretty lady.")
             character.person = persons.PrettyLady()
-        else:
-            Display().write("You ask around, but nobody has heard anything about any assassins.")
 
+        def no_one_cares():
+            Display().write("You ask around, but nobody has heard anything about any assassins.")
+            self.options["a"].add(KillYourselfInFrustration(), weight=50)
+            character.prev_act = self 
+
+        outcomes = Raffle()
+        outcomes.add(assassinated, weight=3)
+        outcomes.add(pretty_lady, weight=1)
+        outcomes.add(no_one_cares, weight=100)
+        outcome = outcomes.get()
+        outcome()
+            
 
 class Attack(Action):
 
     def __init__(self, person):
         self.name = "Attack " + person.pronouns.obj + "."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         if character.person.attack >= character.attack:
@@ -83,6 +105,10 @@ class LickTheGround(Action):
 
     def __init__(self):
         self.name = "Lick the ground."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         pass
@@ -91,19 +117,81 @@ class LickTheGround(Action):
 class LookForWeapons(Action):
     
     def __init__(self):
-        self.name = "Look for weapons"
+        self.name = "Look for weapons."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         Display().write("You find yourself talking to a wealthy war merchant.")
         character.person = persons.WealthyMerchant()
 
 
+class LookForStGeorge(Action):
+    
+    def __init__(self):
+        self.name = "Look for St. George"
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
+
+    def execute(self, character):
+        def lost():
+            Display().write("While looking for St. George, you get lost in "
+                            "your thoughts and realize you stopped paying "
+                            "attention to where you were going.")
+            LeaveInAHuff().execute(character)     
+
+        def find_st_george_at_church():
+            Display().write("You find St. George at the church.")
+            character.move_to(places.church, suppress_message=True)
+            character.person = persons.StGeorge()
+
+        def find_st_george_at_market():
+            Display().write("You find St. George in the market.")
+            character.move_to(places.market, suppress_message=True)
+            character.person = persons.StGeorge()
+
+        def find_st_george_at_streets():
+            Display().write("You find St. George in the streets.")
+            character.move_to(places.streets, suppress_message=True)
+            character.person = persons.StGeorge()
+             
+        def trip_over_a_cat():
+            Display().write("You trip over a cat and break your neck.")
+            character.die()
+
+        outcomes = Raffle()
+        outcomes.add(lost, weight=3)
+        outcomes.add(trip_over_a_cat, weight=1)
+        outcomes.add(find_st_george_at_church, weight=10)
+        outcomes.add(find_st_george_at_market, weight=5)
+        outcomes.add(find_st_george_at_church, weight=5)
+        outcome = outcomes.get()
+        outcome()
+
+
 class KillYourselfInFrustration(Action):
 
     def __init__(self):
         self.name = "Kill yourself in frustration."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
+        if character.place == places.docks:
+            Display().write("You walk into the ocean and are suddenly " 
+                            "inspired to write a novel. You drown.") 
+            character.die()
+            return
+        deaths = ["You perform the ritual of the seppuku.",
+                  "You set yourself on fire and burn to a crisp", 
+                  ]
+        Display().write(random.choice(deaths))
         character.die()
 
 
@@ -115,6 +203,10 @@ class Buy(Action):
     def __init__(self, weapons):
         self.weapon = random.choice(weapons)
         self.name = "Buy a " + self.weapon.name
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         Display().write("You now have a " + self.weapon.name)
@@ -127,6 +219,10 @@ class BuyADrink(Action):
 
     def __init__(self):
         self.name = "Buy a drink."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         Display().write("The blind bartender grumbles as he passes you a drink.")
@@ -137,6 +233,10 @@ class BoastOfYourBravery(Action):
 
     def __init__(self):
         self.name = "Boast of your bravery."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         Display().write(character.person.pronouns.subj.capitalize() +
@@ -147,6 +247,10 @@ class LookForACat(Action):
 
     def __init__(self):
         self.name = "Look for a cat."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         pass
@@ -159,6 +263,10 @@ class LeaveInAHuff(Action):
 
     def __init__(self):
         self.name = "Leave in a huff."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         character.move(speed=1)
@@ -168,6 +276,10 @@ class LeaveInAPuff(Action):
 
     def __init__(self):
         self.name = "Leave in a puff."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         options = places.Place.instances - set([character.place])
@@ -181,6 +293,10 @@ class SingASong(Action):
 
     def __init__(self):
         self.name = "Sing a song."
+        self.options = {"a": Raffle(),
+                        "b": Raffle(),
+                        "c": Raffle(),
+                        "d": Raffle()}
 
     def execute(self, character):
         def a_crowd_gathers():
