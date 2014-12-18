9d9a5746f40037d16dd17bbed31972ae2091e8a4
Sage Berg <sagetberg@gmail.com>
2014-12-17 20:52:39 -0800

    edited combat actions

--- character.py
+++ character.py
@@ -52,9 +52,9 @@ class Character(object):
         """
         self.reset_action_bags()
         if self.threatened:
-            self.bags["c"].add(actions.RunLikeTheDevil(), weight=10)
-            #self.bags["c"].add(actions.LeaveInAHuff, weight=3)
-            #self.bags["c"].add(actions.WaddleLikeGod, weight=1)
+            self.bags["c"].add(actions.RunLikeTheDevil(), weight=9)
+            self.bags["c"].add(actions.LeaveInAHuff(), weight=3)
+            self.bags["c"].add(actions.WaddleLikeGod(), weight=1)
         if self.place and not self.threatened:
             for _ in range(3):
                 self.bags["c"].add(actions.GoTo(self.place))
