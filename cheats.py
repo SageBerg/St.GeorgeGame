bdf77f2e07c8b460477af6c7230869fc61820015
Sage Berg <sagetberg@gmail.com>
2014-12-23 08:55:08 -0800

    added cheat: 'cheat get mushroom' to get you a yellow mushroom

--- cheats.py
+++ cheats.py
@@ -1,3 +1,4 @@
+import items
 import actions
 import places
 import persons
@@ -21,6 +22,8 @@ class Cheat(actions.Action):
                     character.move_to(place)
         if words[0] == "Felicity" and words[1] == "wow":
             persons.fat_lady.attracted += 20
+        if words[0] == "get" and words[1] == "mushroom":
+            character.add_item(items.YellowMushroom())
 
     def execute(self, character):
        self.outcomes.add(Outcome(character,
