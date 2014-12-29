ce80e14ea14b60ec5c1fca0f906bc8ae6f66d1be
Sage Berg <sagetberg@gmail.com>
2014-12-28 21:12:48 -0800

    created

---
+++ monstrosity_actions.py
@@ -0,0 +1,69 @@
+"""
+The St. Georege Game
+monstrosity_actions.py
+Sage Berg
+Created: 28 Dec 2014
+"""
+
+from actions import Action
+from outcome import Outcome
+
+
+class AnnihilateEverything(Action):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Annihilate everything."
+
+    def execute(self, character):
+        self.outcomes.add(Outcome(character,
+            "You start annihilating everything, but the four horsemen of "
+            "apocalypse steal your thunder. You perish in the chaos.",
+            die=True,
+        ), weight=1)
+
+
+class TerrorizeTheKingdom(Action):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Terrorize the kingdom."
+
+    def execute(self, character):
+
+        self.outcomes.add(Outcome(character,
+            "You wreak havoc on a titanic scale, but you eventually fall "
+            "alseep. By the time you wake up, science has advanced so much "
+            "that the world government simply nukes you into oblivion.",
+            die=True,
+        ), weight=1)
+
+
+class GoOnARampage(Action):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Go on a rampage."
+
+    def execute(self, character):
+
+        self.outcomes.add(Outcome(character,
+            "You smash towns, flatten forests, level mountains, and "
+            "ultimately run out of food.",
+            die=True,
+        ), weight=1)
+
+
+class DestroyAllHumanCivilizations(Action):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "Destroy all human civilizations."
+
+    def execute(self, character):
+
+        self.outcomes.add(Outcome(character,
+            "You throw out your arm destroying the first three civilizations "
+            "and an opporunistic hero slays you.",
+            die=True,
+        ), weight=1)
