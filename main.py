09be3e6170c64484a1b08cb40f0ae558c5ba3912
Sage Berg <sagetberg@gmail.com>
2015-01-04 20:31:25 -0800

    added burn all places victory event

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import places
 from character import Character
 import actions
@@ -30,6 +32,18 @@ def main():
         outcome.execute()
         options.add_actions(choices, character, outcome)
         choices.generate_actions(character)
+        
+        if len(places.burned) > 8:  # increase this number for each new 
+                                    # burnable place we add
+            character.alone = False  # might need to add a master_pryo 
+                                     # boolean for the character instead 
+                                     # of using .alone (which makes no sense)
+            print(random.choice(["Some people just like to watch the world "
+                                 "burn. You are one of them.",
+                                 "You are satisfied with how everything has "
+                                 "been burned.",
+                                 ]))
+            print("You win!")
 
 if __name__ == "__main__":
     main()
