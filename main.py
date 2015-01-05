f824c3e90b19d9cc9cd22f7540ef21961e400870
Sage Berg <sagetberg@gmail.com>
2015-01-04 20:40:59 -0800

    added kill all four lords victory condition

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import random
 
 import places
-from character import Character
+import persons
 import actions
 import options
+from character import Character
 from multiple_choice import MultipleChoice
 
+def lords_victory(character):
+    if not persons.lord_arthur.alive and \
+       not persons.lord_bartholomew.alive and \
+       not persons.lord_carlos.alive and \
+       not persons.lord_daniel.alive:
+        character.alone = False
+        print("You have destroyed the establishment and brought about "
+              "a uptopian anarchy... more or less.")
+        print("You win!")
+
+def pyro_victory(character):
+    if len(places.burned) > 8:  # increase this number for each new 
+                                # burnable place we add
+        character.alone = False  # might need to add a master_pryo 
+                                 # boolean for the character instead 
+                                 # of using .alone (which makes no sense)
+        print(random.choice(["Some people just like to watch the world "
+                             "burn. You are one of them.",
+                             "You are satisfied with how everything has "
+                             "been burned.",
+                             ]))
+        print("You win!")
 
 def main():
     """
@@ -32,18 +55,8 @@ def main():
         outcome.execute()
         options.add_actions(choices, character, outcome)
         choices.generate_actions(character)
+        lords_victory(character)
+        pyro_victory(character)
         
-        if len(places.burned) > 8:  # increase this number for each new 
-                                    # burnable place we add
-            character.alone = False  # might need to add a master_pryo 
-                                     # boolean for the character instead 
-                                     # of using .alone (which makes no sense)
-            print(random.choice(["Some people just like to watch the world "
-                                 "burn. You are one of them.",
-                                 "You are satisfied with how everything has "
-                                 "been burned.",
-                                 ]))
-            print("You win!")
-
 if __name__ == "__main__":
     main()
