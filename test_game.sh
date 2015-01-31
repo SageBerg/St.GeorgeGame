9ba2a9518d53a5d027b5d7d924f019dcc7888a5c
Sage Berg <sberg@uoregon.edu>
2015-01-31 11:33:30 -0800

    improved the debugging process by running more trials and redirecting stdin to output.txt so stderr can be seen more easily in the terminal's output

--- test_game.sh
+++ test_game.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for i in `seq 1 10`; do
+echo "WRITING OUTPUT OF 1000 RANDOM PLAYS OF THE GAME TO output.txt"
+echo "stderr prints here on the terminal, so stay tuned for stack traces"
+for i in `seq 1 1000`; do
     python3 gen_random_commands.py
-    python3 main.py < random_commands.txt
+    python3 main.py < random_commands.txt >> output.txt
+    #if [$i % 100 ]
+    #  then
+    #  echo "test case: "
+    #fi
 done
+echo "DONE"
