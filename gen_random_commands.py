a14d782b6b7d9234e6b88082755e22c71b724d31
Sage Berg <sberg@uoregon.edu>
2015-01-30 22:30:17 -0800

    'e' is now an option for, so the void can be tested by the scripts as well

--- gen_random_commands.py
+++ gen_random_commands.py
@@ -3,7 +3,7 @@ import random
 def gen_random_commands():
     f = open("random_commands.txt", "w")
     for i in range(1000):
-        f.write(random.choice(["a", "b", "c", "d"]))
+        f.write(random.choice(["a", "b", "c", "d", "e"]))
         f.write("\n")
     f.close()
 
