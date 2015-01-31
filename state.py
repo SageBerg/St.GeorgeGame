98884922885f9349b75501bacca976ac41d36838
Sage Berg <sberg@uoregon.edu>
2015-01-31 12:42:23 -0800

    persons is no longer optional as an argument

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 class GameState():
     
-    def __init__(self, character, places, persons=None):
+    def __init__(self, character, places, persons):
         self.character = character
         self.places = places
         self.persons = persons
