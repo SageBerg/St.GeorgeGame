6f81092c7b3216bd08c104217a7caaf1212e7b02
Sage Berg <sberg@uoregon.edu>
2015-02-01 11:16:07 -0800

    changed echo statement to more accurately describe what the script does

--- test_game.sh
+++ test_game.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "WRITING OUTPUT OF 1000 RANDOM PLAYS OF THE GAME TO output.txt"
+echo "APPENDING OUTPUT OF 1000 RANDOM PLAYS OF THE GAME TO output.txt"
 echo "stderr prints here on the terminal, so stay tuned for stack traces"
 for i in `seq 1 1000`; do
     python3 gen_random_commands.py
