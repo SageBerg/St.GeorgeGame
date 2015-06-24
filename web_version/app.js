8460a09ebefeeab2d813bc0ed9f014ad8f181515
Sage Berg <sberg@uoregon.edu>
2015-06-23 18:25:49 -0700

    added keyboard interactivity

--- web_version/app.js
+++ web_version/app.js
@@ -1,6 +1,19 @@
 function main() {
-    document.onkeypress = function() {
-        console.log("You pressed a key!");
+    document.onkeypress = function(event) {
+        switch (event.which) {
+            case 97:
+                console.log("you pressed a");
+                break;
+            case 98:
+                console.log("you pressed b");
+                break;
+            case 99:
+                console.log("you pressed c");
+                break;
+            case 100:
+                console.log("you pressed d");
+                break;
+        }
     }
 }
 
