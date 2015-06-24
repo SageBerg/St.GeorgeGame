bab45ef0c3d02b31facd300982f8777304a396fa
Sage Berg <sberg@uoregon.edu>
2015-06-23 21:00:16 -0700

    reorganized web dev files

---
+++ web_version/client/app.js
@@ -0,0 +1,32 @@
+function main() {
+    document.onkeypress = function(event) {
+        switch (event.which) {
+            case 65:
+                console.log("you pressed A");
+                break;
+            case 97:
+                console.log("you pressed a");
+                break;
+            case 66:
+                console.log("you pressed B");
+                break;
+            case 98:
+                console.log("you pressed b");
+                break;
+            case 67:
+                console.log("you pressed C");
+                break;
+            case 99:
+                console.log("you pressed c");
+                break;
+            case 68:
+                console.log("you pressed D");
+                break;
+            case 100:
+                console.log("you pressed d");
+                break;
+        }
+    }
+}
+
+$(document).ready(main);
---
+++ web_version/server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    http = require('http'),
+    port = 3000,
+    app,
+    server;
+
+app = express();
+server = http.createServer(app);
+server.listen(port);
+
+app.use(express.static(__dirname + "/client"));
