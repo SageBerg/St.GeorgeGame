99bd62a7cac5e1b1ee1c674817ecd365dcf16223
Sage Berg <sberg@uoregon.edu>
2015-07-15 07:17:54 -0700

    half way through adding persons content

--- web_version/client/persons.js
+++ web_version/client/persons.js
@@ -1,13 +1,171 @@
 persons = {
-    "pretty_lady": 
+    "assassin":
     {
+        "alive": true,
+        "attack": 6,
+        "attracted": 0,
+        "name": "the assassin",
+        "prefered_attack": "assassinate",
+        "type": "female", 
+    },
+    "assassins":
+    {
+        "alive": true,
+        "attack": 7,
+        "attracted": 0,
+        "name": "the assassins",
+        "prefered_attack": "assassinate",
+        "type": "group", 
+    },
+    "blind_bardtender":
+    {
+        "alive": true,
         "attack": 1,
         "attracted": 0,
-        "name": "the pretty lady",
-    },
-    "blind_bardtender": 
-    {
-        "attack": 1,
         "name": "the blind bartender",
+        "prefered_attack": "kill",
+        "type": "male", 
+    },
+    "eve":
+    {
+        "alive": true,
+        "attack": 4,
+        "attracted": 1,
+        "name": "Lord Carlos' daughter",
+        "prefered_attack": "assassinate",
+        "type": "female", 
+    },
+    "felicity":
+    {
+        "alive": true,
+        "attack": 4,
+        "attracted": 1,
+        "name": "the fat lady",
+        "prefered_attack": "kill",
+        "type": "female", 
+    },
+    "guards":
+    {
+        "alive": true,
+        "attack": 4,
+        "attracted": 0,
+        "name": "the guards",
+        "prefered_attack": "arrest",
+        "type": "group", 
+    },
+    "mermaid":
+    {
+        "alive": true,
+        "attack": 3,
+        "attracted": 1,
+        "name": "the mermaid",
+        "prefered_attack": "drown",
+        "type": "female", 
+    },
+    "mob":
+    {
+        "alive": true,
+        "attack": 9,
+        "attracted": 0,
+        "name": "the angry mob",
+        "prefered_attack": "burn",
+        "type": "group", 
+    },
+    "nymph_queen":
+    {
+        "alive": true,
+        "attack": 10,
+        "attracted": 1,
+        "name": "the nymph queen",
+        "prefered_attack": "hex",
+        "type": "female", 
+    },
+    "olga":
+    {
+        "alive": true,
+        "attack": 1,
+        "attracted": 1,
+        "name": "the pretty lady",
+        "prefered_attack": "kill",
+        "type": "female", 
+    },
+    "other_lunatics":
+    {
+        "alive": true,
+        "attack": -1,
+        "attracted": 0,
+        "name": "the other lunatics",
+        "prefered_attack": "kill",
+        "type": "group", 
+    },
+    "peasant_lass":
+    {
+        "alive": true,
+        "attack": 7,
+        "attracted": 0,
+        "name": "the peasant lass",
+        "prefered_attack": "kill",
+        "sells": ["multi_colored_mushroom", "white_mushroom"],
+        "type": "female", 
+    },
+    "pirates":
+    {
+        "alive": true,
+        "attack": 6,
+        "attracted": 0,
+        "name": "the pirates",
+        "prefered_attack": "kill",
+        "type": "group", 
+    },
+    "simple_peasant":
+    {
+        "alive": true,
+        "attack": -1,
+        "attracted": 0,
+        "name": "the simple peasant",
+        "prefered_attack": "kill",
+        "sells": [],
+        "type": "male", 
+    },
+    "st_george":
+    {
+        "alive": true,
+        "attack": 100,
+        "attracted": 0,
+        "name": "St. George",
+        "prefered_attack": "smite",
+        "sells": [],
+        "type": "male", 
+    },
+    "war_merchant":
+    {
+        "alive": true,
+        "attack": 7,
+        "attracted": 0,
+        "name": "the wealthy war merchant",
+        "prefered_attack": "kill",
+        "sells": ["dagger", "poison_dagger", "cutlass", "jeweled_cutlass", 
+                  "hammer", "iron_hammer"],
+        "type": "male", 
+    },
+    "witch":
+    {
+        "alive": true,
+        "attack": 7,
+        "attracted": 0,
+        "name": "the witch",
+        "prefered_attack": "toad",
+        "sells": [], //potions
+        "type": "female", 
+    },
+    "wizard":
+    {
+        "alive": true,
+        "attack": 7,
+        "attracted": 0,
+        "name": "the wizard",
+        "prefered_attack": "toad",
+        "sells": [],
+        "type": "male", 
     },
 };
