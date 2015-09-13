43cc35b59379cf26133356e89ff33cd1a810de37
Sage Berg <sberg@uoregon.edu>
2015-09-13 11:52:33 -0700

    reduced 'Wander the countryside' probability

--- JavaScript_version/options.js
+++ JavaScript_version/options.js
@@ -389,7 +389,7 @@ function get_place_options(game_state, options) {
         case "countryside":
             raffle.add(options.a, "Go flower picking.", 2);
             raffle.add(options.b, "Tip a cow.", 2);
-            raffle.add(options.c, "Wander the countryside.", 20);
+            raffle.add(options.c, "Wander the countryside.", 10);
             raffle.add(options.d, "Do some farm work.", 2);
             break;
         case "dark_alley":
