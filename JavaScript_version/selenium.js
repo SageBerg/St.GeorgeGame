6337b6da04856d824fc337455996f916e4e948d5
Sage Berg <sberg@uoregon.edu>
2015-10-10 16:21:46 -0700

    created (don't run without commenting out rickroll in client.js)

---
+++ JavaScript_version/selenium.js
@@ -0,0 +1,20 @@
+var functions = require('./functions');
+
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+var NUMBER_OF_TESTS = 100;
+
+driver.get('http://localhost:3000');
+var options = ["a", "b", "c", "d",];
+//e is not included in options because it creates some superfluous output 
+
+for (var i = 0; i < NUMBER_OF_TESTS; i++) {
+    var option = functions.random_choice(options);
+    driver.findElement(By.id(option)).click();
+}
